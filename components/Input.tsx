c4974cd99287eb19fde509aa77cdc917dcb65aec
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-03-13 17:53:13 -0400

    inicio projeto

---
+++ components/Input.tsx
@@ -0,0 +1,23 @@
+
+
+export default function Input(){
+    return (
+       <input
+       className="
+        block
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+        text-white  
+        bg-neutral-700
+        apperance-none
+        fovus:outline-none
+        focus:ring-0
+        peer
+       "
+       />
+    )
+}
