f4146e8ce09cb5036c3b21fe5d6066095bd0ffe4
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-03-15 16:35:15 -0400

    estilização da tela login e sing in

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,19 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 
 const  Auth = () => {
     const [email, setEmial] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    //variant que ira determiar a mensagem a ser exibida
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
            <div className="bg-black w-full h-full lg:bg-opacity-50 ">
@@ -11,19 +21,45 @@ const  Auth = () => {
                     <img src="/images/logo.png" alt="logo" className="h-12" />
                 </nav>
                 <div className="flex justify-center">
-                    <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                    <div className="bg-black bg-opacity-80 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            sing in
+                            {variant === 'login' ? 'Sing in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
+                            
+                            {variant === 'register' && (
+                                <Input
+                                label="Username"
+                                onChange={(ev: any) => setUserName(ev.target.value)}
+                                id="userName"
+                                value={userName}
+                                />
+                            )}
+                
                             <Input
                             label="Email"
-                            onChange={(ev) => setEmial(ev.target.value)}
+                            onChange={(ev: any) => setEmial(ev.target.value)}
                             id="email"
                             type="email"
                             value={email}
                             />
+                            <Input 
+                            label="Password"
+                            onChange={(ev: any) => setPassword(ev.target.value) }
+                            id="password"
+                            type="password"
+                            value={password}
+                            />
                         </div>
+                        <button className="bg-red-600 py-3  text-white rounded-md w-full mt-10   hover:bg-red-700 transition">
+                            {variant === 'login' ? 'Login' : "Sing up"}
+                        </button>
+                        <p className="text-neutral-500 mt-12">
+                            {variant === 'login' ? "First time using netflix?" : "Alredy have account?" }  
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                {variant == 'login' ? 'Create an account' : "Login"}
+                            </span> 
+                        </p>
                     </div>
                 </div>
            </div>
