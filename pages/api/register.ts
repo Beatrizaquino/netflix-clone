d2506a425a85360567333f8dc1cf84e4e6e44722
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-03-27 16:23:47 -0400

    commit test

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import { NextApiRequest, NextApiResponse } from 'next';
-import prismadb from '@/libs/prismadb';
+import prismadb  from '../../lib/prismadb';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
@@ -30,10 +30,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         image: '',
         emailVerified: new Date(),
       }
-    })
+    });
 
     return res.status(200).json(user);
   } catch (error) {
-    return res.status(400).json({ error: `Something went wrong: ${error}` });
+    console.log(error);
+    return res.status(400).end();
   }
 }
